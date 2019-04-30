d0c8ac64c997aec39c95b0ccf28d7988aae6ac4f
BlameFelix <jbraun1998@gmail.com>
2019-04-30 20:15:48 +0200

    Added Java classes for backend

---
+++ BackendObjects/src/Article.java
@@ -0,0 +1,17 @@
+abstract class Article {
+    protected float price;
+    protected int artNr;
+    protected String manufact;
+    protected String artName;
+    protected int count;
+
+    void pullFromDB() {
+
+    }
+    void parseToJson() {
+
+    }
+    void transmit() {
+
+    }
+}
---
+++ BackendObjects/src/Customer.java
@@ -0,0 +1,17 @@
+public class Customer {
+    protected int customerNr;
+    protected String email;
+    protected String name;
+    protected String street;
+    protected String city;
+
+    void pullFromDB() {
+
+    }
+    void parseToJson() {
+
+    }
+    void transmit() {
+
+    }
+}
