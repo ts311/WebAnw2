20d91e78f0a4ce867eaf3381a65c7a0b9deda8ce
BlameFelix <jbraun1998@gmail.com>
2019-05-20 17:12:29 +0200

    Server doesnt work need fix in web.xml

--- Backend/src/main/java/Article.java
+++ Backend/src/main/java/Article.java
@@ -7,4 +7,44 @@ public class Article {
     private String manufact;
     private String artName;
     private int count;
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getArtNr() {
+        return artNr;
+    }
+
+    public void setArtNr(int artNr) {
+        this.artNr = artNr;
+    }
+
+    public String getManufact() {
+        return manufact;
+    }
+
+    public void setManufact(String manufact) {
+        this.manufact = manufact;
+    }
+
+    public String getArtName() {
+        return artName;
+    }
+
+    public void setArtName(String artName) {
+        this.artName = artName;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
