d64a76233d58439769b49956d4fc6203c6677af7
BlameFelix <jbraun1998@gmail.com>
2019-05-25 21:05:40 +0200

    worked on json

--- Backend/src/main/java/de/he/CPU.java
+++ Backend/src/main/java/de/he/CPU.java
@@ -1,31 +1,5 @@
 package de.he;
 
-import org.glassfish.jersey.server.mvc.Viewable;
-import java.net.URI;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Hashtable;
-import java.util.Vector;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import javax.json.Json;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-
-@Path("cpu")
 public class CPU extends Article {
     private int core;
     private int threads;
@@ -42,24 +16,6 @@ public class CPU extends Article {
         this.turbo = turbo;
         this.socket = socket;
         this.tdp = tdp;
-    }
-
-    public CPU getAllCpus() {
-        //Aufruf von Joels methode um die objekte aus der db zu ziehen
-        CPU cpus = new CPU(120, 1, "AMD", "Ryzen 6", 12, 4, 2, 1000, 1, "AM4", 2);
-
-        //String header = "<html>Hallo Welt!</html>";
-        return cpus;
-    }
-
-    @GET
-    @Path("cpu.json")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response allCpus() throws Exception {
-        CPU cpus = new CPU(120, 1, "AMD", "Ryzen 6", 12, 4, 2, 1000, 1, "AM4", 2);
-        // By setting our Vector<EntryModel> into the entity method
-        // Jersey now tries to convert our POJO (Plain Old Java Object) into JSON
-        return Response.status(200).entity(cpus).build();
     }
 
     public int getCore() {
